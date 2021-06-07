a19f538a4923b2996670dca68b3ab56eea7f9ef8
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2021-06-07 17:00:29 +0200

    removeTask interaction added

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ import Section from "./Section";
 import Header from './Header';
 import Container from "./Container";
 
-const tasks = [
-  { id: 1, content: "task1", done: false },
-  { id: 2, content: "task2", done: true },
-];
-
 function App() {
   const [hideDone, setHideDone] = useState(false);
+  const [tasks, setTasks] = useState([
+    { id: 1, content: "task1", done: false },
+    { id: 2, content: "task2", done: true },
+  ]);
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
+  };
+
+  const removeTask = (id) => {
+    setTasks(tasks => tasks.filter(task => task.id != id));
   };
 
   return (
@@ -27,7 +30,9 @@ function App() {
       />
       <Section
         title="Lista zadań"
-        body={<Tasks tasks={tasks} hideDone={hideDone} />}
+        body={
+          <Tasks tasks={tasks} hideDone={hideDone} removeTask={removeTask} />
+        }
         extraHeaderContent={<Buttons
           tasks={tasks}
           hideDone={hideDone}
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./style.css";
 
-const Tasks = ({ tasks, hideDone }) => (
+const Tasks = ({ tasks, hideDone, removeTask }) => (
   <ul className="tasks">
     {tasks.map(task => (
       <li
@@ -13,7 +13,12 @@ const Tasks = ({ tasks, hideDone }) => (
         <span className={`tasks__content${task.done ? " tasks__content--done" : ""}`}>
           {task.content}
         </span>
-        <button className="tasks__button tasks__button--remove">🗑</button>
+        <button
+          className="tasks__button tasks__button--remove"
+          onClick={() => removeTask(task.id)}
+        >
+          🗑
+        </button>
       </li>
     ))}
   </ul>
