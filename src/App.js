3b20f7c865eb932c6cb560db6daefd918de94e99
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-08-04 22:03:09 +0200

    react-router-dom installed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
-import Tasks from "./features/tasks/Tasks";
+import { NavLink, Switch, Route, HashRouter, Redirect } from "react-router-dom";
+import Author from "./features/author/Author";
+import TasksPage from "./features/tasks/TasksPage";
 
-const App = () => <Tasks />;
+const App = () => (
+    <HashRouter>
+        <nav>
+            <ul>
+                <li>
+                    <NavLink activeClassName="active" to="/zadania">Zadania</NavLink>
+                </li>
+                <li>
+                    <NavLink activeClassName="active" to="/autor">O autorze</NavLink>
+                </li>
+            </ul>
+            <Switch>
+                <Route path="/zadania">
+                    <TasksPage />
+                </Route>
+                <Route path="/autor">
+                    <Author />
+                </Route>
+                <Route path="/">
+                    <Redirect to="/zadania" />
+                </Route>
+            </Switch>
+        </nav>
+    </HashRouter>
+);
 
 export default App;
