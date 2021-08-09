797295dabdbf198af643e28a1736280248cf1354
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-08-10 01:36:36 +0200

    fix backgruound problem in single task

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 
 export const SectionContainer = styled.section`
+    background-color: ${({ theme }) => theme.color.background};
     margin-bottom: 10px;
+    padding: 15px;
 `;
 
 export const HeaderContainer = styled.div`
@@ -9,7 +11,6 @@ export const HeaderContainer = styled.div`
     grid-template-columns: 1fr auto auto;
     gap: 10px;
     align-items: center;
-    background-color: ${({ theme }) => theme.color.background};
     padding-right: 10px;
     margin: 0 0 1px 0;
 
@@ -20,7 +21,6 @@ export const HeaderContainer = styled.div`
 `;
 
 export const Header = styled.h2`
-    background-color: ${({ theme }) => theme.color.background};
     padding: 15px;
     font-weight: 700;
     font-size: 20px;
