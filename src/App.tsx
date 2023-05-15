12673753b7269107b76d91e5a4fb0a55d6a0ed90
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2023-05-15 20:18:08 +0200

    build(vite): install and configure dev server vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { Route, HashRouter, Routes, Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { selectLanguage } from 'features/languages/languageSlice';
-import { descriptions } from 'features/languages/descriptions';
-import { TasksPage } from 'features/tasks/tasksPage/TasksPage';
-import { TaskPage } from 'features/tasks/TaskPage';
-import { AuthorPage } from 'features/author/AuthorPage';
-import { Nav } from 'common/nav/Nav';
+import { Route, HashRouter, Routes, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectLanguage } from "features/languages/languageSlice";
+import { descriptions } from "features/languages/descriptions";
+import { TasksPage } from "features/tasks/tasksPage/TasksPage";
+import { TaskPage } from "features/tasks/TaskPage";
+import { AuthorPage } from "features/author/AuthorPage";
+import { Nav } from "common/nav/Nav";
 
-const App = () => {
+export const App = () => {
   const language = useSelector(selectLanguage);
 
   return (
     <HashRouter>
       <Nav
-        tasksPath={'/tasks'}
+        tasksPath={"/tasks"}
         tasksLabel={descriptions[language].tasksPageTitle}
-        authorPath={'/author'}
+        authorPath={"/author"}
         authorLabel={descriptions[language].authorPageTitle}
       />
       <Routes>
@@ -27,5 +27,3 @@ const App = () => {
     </HashRouter>
   );
 };
-
-export default App;
--- src/features/tasks/getExampleTasks.ts
+++ src/features/tasks/getExampleTasks.ts
@@ -1,7 +1,7 @@
 export const getExampleTasks = async () => {
-    const response = await fetch("/todo-list-react/exampleTasks.json");
+    const response = await fetch("/exampleTasks.json");
 
-    if(!response.ok) {
+    if (!response.ok) {
         new Error(response.statusText);
     }
 
