de519744ddc0ad52a4863205df1111dae896a29a
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2023-08-10 01:56:27 +0200

    feat(form_buttons): initially add form-buttons component to molecules, add default config

---
+++ src/ui/molecules/FormButtons.tsx
@@ -0,0 +1,52 @@
+import { styled } from 'styled-components';
+import { Button, ButtonVariants } from 'ui/atoms/Button';
+
+interface ButtonConfig {
+  variant: string;
+  onClick: () => void;
+  children: string;
+  disabled?: boolean;
+}
+
+interface FormButtonsProps {
+  buttonsConfig: ButtonConfig[];
+}
+
+export const FormButtons = ({
+  buttonsConfig = [
+    {
+      variant: 'TEXT',
+      onClick: () => {},
+      children: 'lorem',
+    },
+    {
+      variant: 'TEXT',
+      onClick: () => {},
+      disabled: true,
+      children: 'ipsum',
+    },
+  ],
+}: FormButtonsProps) => {
+  return (
+    <ButtonsWrapper>
+      <>
+        {buttonsConfig.map(({ variant, onClick, disabled, children }) => (
+          <Button variant={variant as ButtonVariants} onClick={onClick} disabled={disabled}>
+            {children}
+          </Button>
+        ))}
+      </>
+    </ButtonsWrapper>
+  );
+};
+
+export const ButtonsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: auto auto;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}) {
+    grid-template-columns: 1fr;
+  }
+`;
+
+FormButtons.displayName = 'FormButtons';
